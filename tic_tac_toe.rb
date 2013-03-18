c761f79689ba95033c3a92f3baf3fdf48472494b
Matthew <mattvleming@gmail.com>
2013-03-17 20:49:19 -0400

    Add methods

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,10 +1,16 @@
 require 'rainbow'
 
 class TicTacToe
-	@@board = [["1", "2", "3"], ["4", "5", "6"], ["7", "8", "9"]]
+	@board = [["1", "2", "3"], ["4", "5", "6"], ["7", "8", "9"]]
+	@players_turn = true
+
+	# is this correct to do?
+	def self.board
+		@board
+	end
 
 	def self.draw_space(space)
-		# add later: colours using rainbow, i.e. space.foreground(:blue) or space.foreground(:red)
+		# add colours later using rainbow, i.e. space.foreground(:blue) or space.foreground(:red)
 		space
 	end
 
@@ -17,7 +23,7 @@ class TicTacToe
 	end
 
 	def self.draw_row(row_num)
-		row = @@board[row_num]
+		row = @board[row_num]
 		self.gap +
 		self.draw_space_with_line(row[0]) + 
 		self.draw_space_with_line(row[1]) + 
@@ -37,7 +43,26 @@ class TicTacToe
 		"\n\n"
 	end
 
+	def self.num_to_space(num)
+		coordinates = []
+		@board.each_with_index do |row, row_index|
+			row.each_with_index do |column, column_index|
+				return coordinates << row_index << column_index if num == column.to_i
+			end
+		end
+	end
+
+	def self.mark_space(coordinates)
+		row = coordinates[0]
+		column = coordinates[1]
+		mark = @players_turn ? "x" : "o"
+		@board[row][column] = mark
+	end
+
 	def self.players_turn()
+		num = gets[0].to_i
+		coordinates = self.num_to_space(num)
+		self.mark_space(coordinates)
 	end
 
 	def self.comps_turn()
--- tic_tac_toe_tests.rb
+++ tic_tac_toe_tests.rb
@@ -1,9 +1,38 @@
 require 'test/unit'
-require 'tic_tac_toe.rb'
+require './tic_tac_toe.rb'
 
 class TicTacToeTest < Test::Unit::TestCase
 
-	def test_
+	def test_draw_space
+		assert_equal "x", TicTacToe.draw_space("x")
+	end
+
+	def test_draw_space_with_line
+		assert_equal "3 | ", TicTacToe.draw_space_with_line("3")
+	end
+
+	def test_draw_row
+		assert_equal "  7 | 8 | 9", TicTacToe.draw_row(2)
+	end
+
+	def test_draw_row_with_line
+		assert_equal "  4 | 5 | 6\n  ---------\n", TicTacToe.draw_row_with_line(1)
+	end
+
+	def test_draw_board
+		assert_equal "\n  1 | 2 | 3\n  ---------\n  4 | 5 | 6\n  ---------\n  7 | 8 | 9\n\n", TicTacToe.draw_board()
+	end
+
+	def test_num_to_space
+		assert_equal [0, 1], TicTacToe.num_to_space(2)
+		assert_equal [2, 2], TicTacToe.num_to_space(9)
+	end
+
+	def test_mark_space
+		TicTacToe.mark_space([1, 2])
+		assert_equal [["1", "2", "3"], ["4", "5", "x"], ["7", "8", "9"]], TicTacToe.board
+		TicTacToe.mark_space([1, 2])
+		assert_equal [["1", "2", "3"], ["4", "5", "x"], ["7", "8", "9"]], TicTacToe.board
 	end
 
 end
